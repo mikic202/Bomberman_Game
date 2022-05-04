76b752f3ff966ffbbc2fb991d4babb1a98eba9bc
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-05-04 09:58:34 +0200

    defined all bomb class methods

---
+++ Bomberman/Items/Bomb.cpp
@@ -0,0 +1,44 @@
+#include "Bomb.h"
+#include "../Consts.h"
+#include <cmath>
+
+
+Bomb::Bomb(std::vector<int> position, int radius, int delay, int strength)
+{
+	set_name("Bomb");
+	set_position(position);
+	set_radius(radius);
+	set_delay(delay);
+	set_strength(strength);
+}
+
+void Bomb::set_radius(int new_radius)
+{
+	radius_ = new_radius;
+}
+
+int Bomb::radius()
+{
+	return radius_;
+}
+
+void Bomb::set_delay(int new_delay)
+{
+	if (new_delay > MAX_EXPLOSION_DELAY) throw(std::exception("Too big Delay"));
+	delay_ = new_delay;
+}
+
+int Bomb::delay()
+{
+	return delay_;
+}
+
+void Bomb::set_strength(int new_strength)
+{
+	strength_ = new_strength;
+}
+
+int Bomb::strength()
+{
+	return strength_;
+}
