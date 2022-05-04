878b18ecb986fb1fbb6ff6e579557bc12d247f0b
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-05-04 10:00:49 +0200

    defined all methods of Box class

---
+++ Bomberman/Items/Box.cpp
@@ -0,0 +1,32 @@
+#include "Box.h"
+
+
+Box::Box(std::vector<int> position, int hits_to_brake)
+{
+	set_hits_to_brake(hits_to_brake);
+	set_position(position);
+}
+
+void Box::set_hits_to_brake(int new_hits)
+{
+	if (new_hits <= 0) throw(std::exception("Can't set box to be broken"));
+	hits_to_brake_ = new_hits;
+}
+
+int Box::hits_to_brake()
+{
+	return hits_to_brake_;
+}
+
+bool Box::hit(int strength)
+{
+	hits_to_brake_ -= strength;
+	return chek_if_broken_();
+}
+
+bool Box::chek_if_broken_()
+{
+	if (hits_to_brake_ > 0)
+		return true;
+	return false;
+}
