aa08ffc8ae7d0dc146bba9da7da5ff2a9c9f60e3
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-05-04 09:38:24 +0200

    added consts file for all constants

---
+++ Bomberman/Consts.h
@@ -0,0 +1,12 @@
+#ifndef Consts_H
+#define Consts_H
+#include <vector>
+
+
+const int DIMENSIONS = 2;
+const std::vector<int> MAX_SIZE = { 1000, 1000 };
+const int MAX_EXPLOSION_DELAY = 10;
+
+
+#endif // !Consts_H
+
