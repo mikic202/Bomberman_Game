33c990f1b8e8ab98eb8f20701d9ba8c19dcffe63
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-05-07 16:46:47 +0200

    Added box class constructor

--- Bomberman/Items/Wall.cpp
+++ Bomberman/Items/Wall.cpp
@@ -5,3 +5,10 @@ sf::FloatRect Wall::get_global_bounds()
 {
 	return item_sprite_.getGlobalBounds();
 }
+
+Wall::Wall(std::vector<int> position, sf::Vector2f scale, std::string image_path)
+{
+	set_position(position);
+	rescale(scale);
+	load_immage_from_file(image_path);
+}
--- Bomberman/Items/Wall.h
+++ Bomberman/Items/Wall.h
@@ -6,6 +6,8 @@
 class Wall : public Item
 {
 public:
+	Wall() {};
+	Wall(std::vector<int> position, sf::Vector2f scale, std::string image_path);
 	sf::FloatRect get_global_bounds();
 };
 
