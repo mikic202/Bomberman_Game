b720171d70ab57caa363df2b8d0a72ad1e6b4af3
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-05-08 10:42:55 +0200

    added players vector

--- Bomberman/Game Board/GameBoard.cpp
+++ Bomberman/Game Board/GameBoard.cpp
@@ -23,3 +23,14 @@ std::shared_ptr<Item> GameBoard::item(int item_position)
 {
 	return items_on_board_[item_position];
 }
+
+std::vector<std::shared_ptr<Player>> GameBoard::players()
+{
+	return players_;
+}
+
+void GameBoard::add_player(Player new_player)
+{
+	std::shared_ptr<Player> ptr = std::make_shared<Player>(new_player);
+	players_.push_back(std::move(ptr));;
+}
--- Bomberman/Game Board/GameBoard.h
+++ Bomberman/Game Board/GameBoard.h
@@ -3,6 +3,7 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
 #include"../Items/Item.h"
+#include"../Entities/Player/Player.h"
 
 class GameBoard
 {
@@ -10,19 +11,23 @@ protected:
 	sf::Texture background_;
 	std::vector<std::shared_ptr<Item> > items_on_board_;
 	int max_boxes_on_board_;
+	std::vector<std::shared_ptr<Player> > players_;
 public:
 	void load_bacground(std::string backgroung_image);
 	sf::Texture bacground();
 	template<typename T> void add_item(T new_item);
 	std::vector<std::shared_ptr<Item> > items();
 	std::shared_ptr<Item> item(int item_position);
+	std::vector<std::shared_ptr<Player> > players();
+	void add_player(Player new_player);
 };
 
 
 template<typename T> 
 inline void GameBoard::add_item(T new_item)
 {
-	items_on_board.push_back(new_item);
+	std::shared_ptr<Item> ptr = std::make_shared<T>(new_item);
+	items_on_board_.push_back(std::move(ptr));
 }
 
 
