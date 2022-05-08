15eb0983b97e69b8105c33ac7c5c2defe13bae79
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-05-08 20:52:03 +0200

    defined most of the SToryModeBoard methods

--- Bomberman/Game Board/StoryModeBoard.cpp
+++ Bomberman/Game Board/StoryModeBoard.cpp
@@ -3,5 +3,33 @@
 
 StoryModeBoard::StoryModeBoard(int levels_pased, int number_of_players)
 {
+    max_boxes_on_board_ = dificulty_(levels_pased);
+    /*for (int i = 1; i <= number_of_players; i++)
+    {
+        players_.push_back(Player());
+    }*/
+}
 
+sf::Vector2f StoryModeBoard::door_position() const
+{
+    return door_sprite_.getPosition();
+}
+
+sf::FloatRect StoryModeBoard::get_door_global_bounds()
+{
+    return door_sprite_.getGlobalBounds();
+}
+
+int StoryModeBoard::dificulty_(int levels_pased) const
+{
+    int previous = 0;
+    for (std::pair<int, int> dif : dificulty_map_)
+    {
+        if (dif.first > levels_pased)
+        {
+            break;
+        }
+        previous = dif.first;
+    }
+    return dificulty_map_.at(previous);
 }
--- Bomberman/Game Board/StoryModeBoard.h
+++ Bomberman/Game Board/StoryModeBoard.h
@@ -10,7 +10,7 @@ private:
 	int max_num_boxes_;
 	sf::Sprite door_sprite_;
 	const int where_door_can_ocure = 50;
-	const std::map<int, int> dificulty_map_ {{0, 20}, {5, 30}, {10, 40}};// first - levels pased, second - boxes on board
+	std::map<int, int> dificulty_map_ {{0, 20}, {5, 30}, {10, 40}};// first - levels pased, second - boxes on board
 	void generate_board_(int boxes_number);
 	int dificulty_(int levels_pased) const;
 public:
