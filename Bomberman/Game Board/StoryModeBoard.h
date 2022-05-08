a3779bc6e386dbc1695fa7b0c21b11079300b27d
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-05-08 20:36:30 +0200

    added dificulty map with example values

--- Bomberman/Game Board/StoryModeBoard.h
+++ Bomberman/Game Board/StoryModeBoard.h
@@ -1,6 +1,7 @@
 #ifndef StoryModeBoard_H
 #define StoryModeBoard_H
 #include "GameBoard.h"
+#include <map>
 
 
 class StoryModeBoard : public GameBoard
@@ -9,6 +10,7 @@ private:
 	int max_num_boxes_;
 	sf::Sprite door_sprite_;
 	const int where_door_can_ocure = 50;
+	const std::map<int, int> dificulty_map_ {{0, 20}, {5, 30}, {10, 40}};
 	void generate_board(int boxes_number);
 public:
 	StoryModeBoard(int levels_pased, int number_of_players);
