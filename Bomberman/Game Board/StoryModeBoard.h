85d93626c1532257636221c731b71353c56b1067
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-05-08 10:36:10 +0200

    declared StoryModeBoard methods

--- Bomberman/Game Board/StoryModeBoard.h
+++ Bomberman/Game Board/StoryModeBoard.h
@@ -1,1 +1,19 @@
-#pragma once
+#ifndef StoryModeBoard_H
+#define StoryModeBoard_H
+#include "GameBoard.h"
+
+
+class StoryModeBoard : public GameBoard
+{
+private:
+	int dificulty_;
+	sf::Sprite door_sprite_;
+public:
+	StoryModeBoard(int levels_pased);
+	int dificulty() const;
+	sf::Vector2f door_position() const;
+	sf::FloatRect get_door_global_bounds();
+};
+
+
+#endif // !StoryModeBoard_H
