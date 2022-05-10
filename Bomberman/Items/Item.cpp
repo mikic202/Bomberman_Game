9ed6d7769c51aa61888d845b4d647e1eb219a35a
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-05-10 21:18:38 +0200

    temporartly fixed problems with sprites

--- Bomberman/Items/Item.cpp
+++ Bomberman/Items/Item.cpp
@@ -20,7 +20,7 @@ sf::Sprite Item::item_sprite() const
 
 void Item::load_immage_from_file(std::string image_path)
 {
-    item_sprite_.setOrigin(sprite_size_[0] / 2, sprite_size_[1] / 2);
+    //item_sprite_.setOrigin(sprite_size_[0] / 2, sprite_size_[1] / 2);
     sf::Texture item_texture;
 
     if (!item_texture.loadFromFile(image_path))
