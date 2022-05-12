ea99dd3e99c08ed0ebf6bb326b967299cbb62e5e
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-05-12 18:22:47 +0200

    quickly tested sprites with custom textures

--- Bomberman/testng_merging.cpp
+++ Bomberman/testng_merging.cpp
@@ -1,11 +1,18 @@
 #include <SFML/Graphics.hpp>
+#include"Items/Bomb.h"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
+    sf::RenderWindow window(sf::VideoMode(700, 700), "SFML works!");
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Green);
+    Bomb bomb({ 10, 10 }, 5, 6, 6, {0.5, 0.5});
 
+    sf::Sprite item_sprite_;
+    sf::Texture item_texture;
+
+
+    std::cout << "a";
     while (window.isOpen())
     {
         sf::Event event;
@@ -16,7 +23,10 @@ int main()
         }
 
         window.clear();
-        window.draw(shape);
+        bomb.draw_to(window);
+        //window.draw(bomb.item_sprite_);
+        //window.draw(item_sprite_);
+        //window.draw(shape);
         window.display();
     }
 
