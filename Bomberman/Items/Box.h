794ee3cb46784dd9522aec2a49cfe4ab6c0de044
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-05-12 19:19:30 +0200

    added temporary texture path

--- Bomberman/Items/Box.h
+++ Bomberman/Items/Box.h
@@ -8,6 +8,7 @@ class Box : public Wall
 private:
 	int hits_to_brake_ = 1;
 	bool chek_if_broken_();
+	std::string box_texture_path_ = "Assets/Bomb.png"
 public:
 	Box() {  };
 	Box(sf::Vector2f position, int hits_to_brake, sf::Vector2f scale, std::string image_path);
