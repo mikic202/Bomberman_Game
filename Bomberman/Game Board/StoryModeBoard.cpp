d26c30c094124ae8672933a92fdec3a46984e50d
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-05-12 20:04:33 +0200

    partialy defined board generation method

--- Bomberman/Game Board/StoryModeBoard.cpp
+++ Bomberman/Game Board/StoryModeBoard.cpp
@@ -1,34 +1,6 @@
 #include "StoryModeBoard.h"
 
 
-int StoryModeBoard::box_num_(int level_num) const
-{
-    int previous = 0;
-    for (std::pair<int, int> dif : dificulty_map_)
-    {
-        if (dif.first > level_num)
-        {
-            break;
-        }
-        previous = dif.first;
-    }
-    return dificulty_map_.at(previous);
-}
-
-int StoryModeBoard::max_box_strength(int level_num) const
-{
-    int previous = 0;
-    for (std::pair<int, int> dif : box_strength_map_)
-    {
-        if (dif.first > level_num)
-        {
-            break;
-        }
-        previous = dif.first;
-    }
-    return box_strength_map_.at(previous);
-}
-
 StoryModeBoard::StoryModeBoard(int levels_pased, int number_of_players)
 {
     level_number_ = levels_pased;
@@ -46,5 +18,41 @@ sf::Vector2f StoryModeBoard::door_position() const
 sf::FloatRect StoryModeBoard::get_door_global_bounds()
 {
     return door_sprite_.getGlobalBounds();
+}
+
+void StoryModeBoard::generate_board_()
+{
+    int box_num = box_num_(level_number_);
+    int max_box_strength = max_box_strength_(level_number_);
+
+    place_walls_(size_[0], size_[1]);
+}
+
+int StoryModeBoard::box_num_(int level_num) const
+{
+    int previous = 0;
+    for (std::pair<int, int> dif : dificulty_map_)
+    {
+        if (dif.first > level_num)
+        {
+            break;
+        }
+        previous = dif.first;
+    }
+    return dificulty_map_.at(previous);
+}
+
+int StoryModeBoard::max_box_strength_(int level_num) const
+{
+    int previous = 0;
+    for (std::pair<int, int> dif : box_strength_map_)
+    {
+        if (dif.first > level_num)
+        {
+            break;
+        }
+        previous = dif.first;
+    }
+    return box_strength_map_.at(previous);
 }
 
--- Bomberman/Game Board/StoryModeBoard.h
+++ Bomberman/Game Board/StoryModeBoard.h
@@ -12,9 +12,10 @@ private:
 	const int where_door_can_ocure = 50;
 	std::map<int, int> dificulty_map_ {{0, 20}, {5, 30}, {10, 40}};// first - levels pased, second - boxes on board
 	std::map<int, int> box_strength_map_{ {0, 1}, {5, 2}, {10, 3} };
-	void generate_board_(int boxes_number);
+	void generate_board_();
 	int box_num_(int level_num) const;
-	int max_box_strength(int level_num) const;
+	int max_box_strength_(int level_num) const;
+	const std::vector<int> size_{ 50, 100 };
 public:
 	StoryModeBoard(int level_number, int number_of_players);
 	sf::Vector2f door_position() const;
