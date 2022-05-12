d00b968e836e6350dfa7696986b1b6a7667825a9
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-05-12 18:36:40 +0200

    added tests for Item class

--- Bomberman/testng_merging.cpp
+++ Bomberman/testng_merging.cpp
@@ -1,34 +1,34 @@
-#include <SFML/Graphics.hpp>
-#include"Items/Bomb.h"
-
-int main()
-{
-    sf::RenderWindow window(sf::VideoMode(700, 700), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-    Bomb bomb({ 10, 10 }, 5, 6, 6, {0.5, 0.5});
-
-    sf::Sprite item_sprite_;
-    sf::Texture item_texture;
-
-
-    std::cout << "a";
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        bomb.draw_to(window);
-        //window.draw(bomb.item_sprite_);
-        //window.draw(item_sprite_);
-        //window.draw(shape);
-        window.display();
-    }
-
-    return 0;
-}
+//#include <SFML/Graphics.hpp>
+//#include"Items/Bomb.h"
+//
+//int main()
+//{
+//    sf::RenderWindow window(sf::VideoMode(700, 700), "SFML works!");
+//    sf::CircleShape shape(100.f);
+//    shape.setFillColor(sf::Color::Green);
+//    Bomb bomb({ 10, 10 }, 5, 6, 6, {0.5, 0.5});
+//
+//    sf::Sprite item_sprite_;
+//    sf::Texture item_texture;
+//
+//
+//    std::cout << "a";
+//    while (window.isOpen())
+//    {
+//        sf::Event event;
+//        while (window.pollEvent(event))
+//        {
+//            if (event.type == sf::Event::Closed)
+//                window.close();
+//        }
+//
+//        window.clear();
+//        bomb.draw_to(window);
+//        //window.draw(bomb.item_sprite_);
+//        //window.draw(item_sprite_);
+//        //window.draw(shape);
+//        window.display();
+//    }
+//
+//    return 0;
+//}
