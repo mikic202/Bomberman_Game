ee47e01382cdd76d029176774341207ac8e9b693
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-05-13 12:13:00 +0200

    added new constructor that relies on external texture

--- Bomberman/Items/Wall.cpp
+++ Bomberman/Items/Wall.cpp
@@ -6,6 +6,14 @@ sf::FloatRect Wall::get_global_bounds()
 	return item_sprite_.getGlobalBounds();
 }
 
+Wall::Wall(sf::Vector2f position, sf::Vector2f scale, sf::Texture& wall_texture)
+{
+	set_position(position);
+	rescale(scale);
+	item_sprite_.setTexture(wall_texture);
+	//load_immage_from_file(wall_texture_path_);
+}
+
 Wall::Wall(sf::Vector2f position, sf::Vector2f scale)
 {
 	set_position(position);
--- Bomberman/Items/Wall.h
+++ Bomberman/Items/Wall.h
@@ -1,15 +1,17 @@
 #ifndef Wall_H
 #define Wall_H
 #include "Item.h"
+#include "../Consts.h"
 
 
 class Wall : public Item
 {
 private:
-	std::string wall_texture_path_ = "Assets/Bomb.png";
+	std::string wall_texture_path_ = BOMB_PATH;
 public:
 	Wall() {};
 	Wall(sf::Vector2f position, sf::Vector2f scale);
+	Wall(sf::Vector2f position, sf::Vector2f scale, sf::Texture& wall_texture);
 	sf::FloatRect get_global_bounds();
 };
 
