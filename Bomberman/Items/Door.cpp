61c819880c2a3eb485d61039823c99da54ed3210
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-05-13 21:53:40 +0200

    added and defined Door class

---
+++ Bomberman/Items/Door.cpp
@@ -0,0 +1,17 @@
+#include "Door.h"
+
+
+Door::Door(sf::Vector2f position, sf::Vector2f scale, sf::Texture& wall_texture)
+{
+	set_position(position);
+	rescale(scale);
+	item_sprite_.setTexture(wall_texture);
+	//load_immage_from_file(wall_texture_path_);
+}
+
+Door::Door(sf::Vector2f position, sf::Vector2f scale)
+{
+	set_position(position);
+	rescale(scale);
+	load_immage_from_file(door_texture_path_);
+}
--- Bomberman/Items/Door.h
+++ Bomberman/Items/Door.h
@@ -1,5 +1,18 @@
 #ifndef Door_H
 #define Door_H
+#include "Item.h"
+#include "../Consts.h"
+
+
+class Door : public Item
+{
+private:
+	std::string door_texture_path_ = DOOR_PATH;
+public:
+	Door() {};
+	Door(sf::Vector2f position, sf::Vector2f scale);
+	Door(sf::Vector2f position, sf::Vector2f scale, sf::Texture& wall_texture);
+};
 
 
 #endif
