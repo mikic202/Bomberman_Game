eaf2364d9f9b6d18cf4922a84c2e1da05282a077
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-05-14 09:34:13 +0200

    added place boxes method

--- Bomberman/Game Board/GameBoard.cpp
+++ Bomberman/Game Board/GameBoard.cpp
@@ -70,9 +70,27 @@ void GameBoard::place_walls_(int size_x, int size_y)
 void GameBoard::generate_board_()
 {
 	place_walls_(MAX_SIZE[0], MAX_SIZE[1]);
+	place_boxes_(MAX_SIZE[0], MAX_SIZE[1]);
 }
 
 void GameBoard::place_boxes_(int size_x, int size_y)
 {
-
+	if (!box_texture_.loadFromFile(BOX_PATH))
+	{
+		throw (FliePathException());
+	}
+	if (size_y == -1) size_y = size_x;
+	int random_nuber_if_box;
+	for (int y = 1; y <= size_y; y++)
+	{
+		for (int x = 1; x <= size_x; x++)
+		{
+			random_nuber_if_box = std::rand() % 2 + 1;
+			if (((x % 2 == 1 && y % 2 == 0) || (y % 2 == 1)) && random_nuber_if_box == 1)
+			{
+				Box box({ float((x - 1) * GRID_SLOT_SIZE), float((y - 1) * GRID_SLOT_SIZE) }, 1, { .14286, .14286 }, box_texture_);
+				add_item(box);
+			}
+		}
+	}
 }
