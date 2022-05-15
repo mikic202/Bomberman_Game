30e0d4609e0c64aa7a6e508c63b85b76a0aa15c1
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-05-15 19:40:37 +0200

    added tests for board classes

--- Bomberman/tests/bomberman_tests.cpp
+++ Bomberman/tests/bomberman_tests.cpp
@@ -2,6 +2,8 @@
 #include "catch.hpp"
 #include "../Items/Bomb.h"
 #include"../Items/Box.h"
+#include"../Game Board/StoryModeBoard.h"
+#include"../Game Board/VersusModeBoard.h"
 
 
 TEST_CASE("Tests for Item class")
@@ -106,5 +108,62 @@ TEST_CASE("Tsting Box class")
 		REQUIRE(!box.hit(2));
 
 		REQUIRE(box.hit(4));
+	}
+}
+
+
+TEST_CASE("Tests for GameBoard class")
+{
+	SECTION("Setters and getters tests tests")
+	{
+		GameBoard board;
+		Player player(1, 2, 3, 4, 5, 6);
+
+		board.add_player(player);
+		REQUIRE(board.players()[0] -> get_hp() == 6);
+
+	}
+
+	SECTION("add_items tests")
+	{
+		GameBoard board;
+
+		board.add_item(Wall());
+		REQUIRE(board.items().size() == 1);
+	}
+}
+
+
+TEST_CASE("Tests for StoryModeBoard class")
+{
+	SECTION("Constructor tests")
+	{
+		StoryModeBoard board(4, 1);
+
+		REQUIRE(board.level_number() == 4);
+
+		REQUIRE(board.items().size() <= 20*50/4 + 40);
+	}
+
+	SECTION("Getters tests")
+	{
+		StoryModeBoard board(4, 1);
+
+		REQUIRE(board.door_position().x >= 50/2);
+
+		
+	}
+}
+
+
+TEST_CASE("Tests for VersusModeBoard")
+{
+	SECTION("Constructor tests")
+	{
+		VersusModeBoard board(16);
+
+		REQUIRE(board.size() == 16);
+
+		REQUIRE(board.items().size() >= 16 * 16 / 4);
 	}
 }
