df5ae62465718efd78006e533ac8cbf767de1c4d
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-05-19 17:04:19 +0200

    defined all of the Explosion methods

--- Bomberman/Items/Explosion.cpp
+++ Bomberman/Items/Explosion.cpp
@@ -1,1 +1,21 @@
 #include "Explosion.h"
+
+
+Explosion::Explosion(sf::Vector2f position, sf::Vector2f scale)
+{
+	set_position(position);
+	rescale(scale);
+}
+
+Explosion::Explosion(sf::Vector2f position, sf::Vector2f scale, sf::Texture& explosion_texture)
+{
+	set_position(position);
+	rescale(scale);
+	item_sprite_.setTexture(explosion_texture);
+}
+
+bool Explosion::is_col_player(Player& player)
+{
+	//return get_global_bounds().intersects(player.get_global_bounds());
+	return true;
+}
--- Bomberman/Items/Explosion.h
+++ Bomberman/Items/Explosion.h
@@ -1,13 +1,16 @@
 #ifndef Explosion_H
 #define Explosion_H
 #include "Wall.h"
+#include"../Entities/Player/Player.h"
 
 
 class Explosion : public Wall
 {
+public:
 	Explosion() {};
 	Explosion(sf::Vector2f position, sf::Vector2f scale);
-	Explosion(sf::Vector2f position, sf::Vector2f scale, sf::Texture explosion_texture);
+	Explosion(sf::Vector2f position, sf::Vector2f scale, sf::Texture& explosion_texture);
+	bool is_col_player(Player& player);
 };
 
 
