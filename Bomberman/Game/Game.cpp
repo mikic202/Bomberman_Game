fa60d74b35ded668117bdd2c8940911e6784fe65
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-05-21 15:32:30 +0200

    added basic player class for testing

--- Bomberman/Game/Game.cpp
+++ Bomberman/Game/Game.cpp
@@ -101,3 +101,39 @@ std::vector<int> Game::load_game(int save_number, char type)
     save_file.close();
     return std::vector<int> {level_number, std::stoi(line)};
 }
+
+
+
+class Player2
+{
+public:
+    Player2(sf::Vector2f size)
+    {
+        player.setSize(size);
+        player.setFillColor(sf::Color::Green);
+    }
+
+    void draw_to(sf::RenderWindow& window)
+    {
+        window.draw(player);
+    }
+    void move(sf::Vector2f distance)
+    {
+        player.move(distance);
+    }
+    void set_position(sf::Vector2f position)
+    {
+        player.setPosition(position);
+    }
+
+    int getX() {
+        return player.getPosition().x;
+    }
+
+    int getY() {
+        return player.getPosition().y;
+    }
+
+private:
+    sf::RectangleShape player;
+};
