120e22cb83dd7b9e2118103a45eb26a4fcb90e89
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-05-21 15:36:26 +0200

    added Player clsss for testing

--- Bomberman/Game/Game.h
+++ Bomberman/Game/Game.h
@@ -15,12 +15,47 @@ private:
 	void play_story_(int save_number, bool new_game);
 	void play_coop_();
 	void play_versus_();
-	void move_player_(Player &player);
+	void move_player_(Player2 &player);
 	void save_game_(int save_number, char type, int leve_number, int points);
 	std::vector<int> load_game(int save_number, char type);
 public:
 	Game() {};
 	void play(int save_number, char type, bool new_game);
+};
+
+
+class Player2
+{
+public:
+    Player2(sf::Vector2f size)
+    {
+        player.setSize(size);
+        player.setFillColor(sf::Color::Green);
+    }
+
+    void draw_to(sf::RenderWindow& window)
+    {
+        window.draw(player);
+    }
+    void move(sf::Vector2f distance)
+    {
+        player.move(distance);
+    }
+    void set_position(sf::Vector2f position)
+    {
+        player.setPosition(position);
+    }
+
+    int getX() {
+        return player.getPosition().x;
+    }
+
+    int getY() {
+        return player.getPosition().y;
+    }
+
+private:
+    sf::RectangleShape player;
 };
 
 
