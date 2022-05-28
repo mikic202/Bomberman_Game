effc22d27f9bf3fb07c1655c8e60043a6509e0ef
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-05-28 21:37:16 +0200

    added explosion duration

--- Bomberman/Items/Explosion.cpp
+++ Bomberman/Items/Explosion.cpp
@@ -13,3 +13,8 @@ Explosion::Explosion(sf::Vector2f position, sf::Vector2f scale, sf::Texture& exp
 	rescale(scale);
 	item_sprite_.setTexture(explosion_texture);
 }
+
+bool Explosion::did_end()
+{
+	return(duration_ < std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - placement_time_).count());
+}
--- Bomberman/Items/Explosion.h
+++ Bomberman/Items/Explosion.h
@@ -2,14 +2,19 @@
 #define Explosion_H
 #include "Wall.h"
 #include"../Entities/Player/Player.h"
+#include <chrono>
 
 
 class Explosion : public Wall
 {
+private:
+	const int duration_ = 1;
+	std::chrono::time_point<std::chrono::steady_clock> placement_time_;
 public:
 	Explosion() {};
 	Explosion(sf::Vector2f position, sf::Vector2f scale);
 	Explosion(sf::Vector2f position, sf::Vector2f scale, sf::Texture& explosion_texture);
+	bool did_end();
 };
 
 
