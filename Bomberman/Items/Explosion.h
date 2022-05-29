aae55ad5d43bad9f02fe2a04fd630c1f75b8c7ac
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-05-29 18:49:01 +0200

    changed explosion duration

--- Bomberman/Items/Explosion.h
+++ Bomberman/Items/Explosion.h
@@ -8,7 +8,7 @@
 class Explosion : public Wall
 {
 private:
-	const int duration_ = 2;
+	const int duration_ = 1;
 	std::chrono::time_point<std::chrono::steady_clock> explosion_time_;
 public:
 	Explosion() {};
