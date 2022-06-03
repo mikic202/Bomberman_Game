5ca9889a2fc53694d9e4549ee84e09bec3024979
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-06-03 21:48:20 +0200

    improved how often boxes spawn

--- Bomberman/Game Board/StoryModeBoard.h
+++ Bomberman/Game Board/StoryModeBoard.h
@@ -12,12 +12,12 @@ private:
 	Door door_;
 	sf::Texture door_texture_;
 	const int where_door_can_ocure = 50;
-	std::map<int, int> dificulty_map_ {{0, 60}, {5, 100}, {10, 140}};// first - levels pased, second - boxes on board
+	std::map<int, int> dificulty_map_ {{0, 80}, {5, 110}, {10, 140}};// first - levels pased, second - boxes on board
 	std::map<int, int> box_strength_map_{ {0, 1}, {5, 2}, {10, 3} };
 	void generate_board_(sf::Texture& wall_texture, sf::Texture& box_texture, sf::Texture& door_texture);
 	int box_num_(int level_num) const;
 	int max_box_strength_(int level_num) const;
-	const std::vector<int> size_{ 30, 10 };
+	const std::vector<int> size_{ 30, 11 };
 	void place_boxes_(int size_x, int size_y, sf::Texture& box_texture);
 	void place_door_(sf::Texture& door_texture);
 public:
