fcf3b828703004166c7dbf4ef13e9b41d355c18c
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-06-04 09:09:57 +0200

    czhanged order of parameters in constructor

--- Bomberman/Entities/Enemy/Enemy.h
+++ Bomberman/Entities/Enemy/Enemy.h
@@ -14,7 +14,7 @@ class Enemy : public Entity
 public:
 	//Enemy(int pos_x, int pos_y, unsigned int size_x, unsigned int size_y, int speed, int damage_to_player);
 	Enemy();
-	Enemy(sf::Vector2f position, sf::Texture& texture, sf::Vector2f);
+	Enemy(sf::Vector2f position, sf::Vector2f scale, sf::Texture& texture);
 	int get_damage_to_player();
 
 	void set_damage_to_player(int new_damage);
