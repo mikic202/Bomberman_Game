4d952a673f261d91be055797ed2e92f493c0567d
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-07-08 21:13:45 +0200

    added checking if button is pressed

--- Bomberman/Menu2/Button.cpp
+++ Bomberman/Menu2/Button.cpp
@@ -5,5 +5,19 @@ Button::Button(sf::Vector2f position, sf::Vector2f size, sf::Color button_color,
 	butto_rect_.setPosition(position);
 	butto_rect_.setSize(size);
 	butto_rect_.setFillColor(button_color);
-	
+	button_text_ = text;
 }
+
+void Button::draw_to(sf::RenderWindow& window)
+{
+	window.draw(butto_rect_);
+	window.draw(button_text_);
+}
+
+bool Button::is_pressed(sf::RenderWindow& window)
+{
+	sf::Vector2i mouse_pos = sf::Mouse::getPosition(window);
+	return butto_rect_.getGlobalBounds().contains({ mouse_pos.x, mouse_pos.y});
+}
+
+
