125818255a35d97919359b0a8d7981c16eaf77ea
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-07-08 20:45:27 +0200

    added basic menu class functionalities

--- Bomberman/Menu2/Menu.cpp
+++ Bomberman/Menu2/Menu.cpp
@@ -1,1 +1,12 @@
 #include "Menu.h"
+
+
+void Menu::open_menu()
+{
+	while (is_menu_open_)
+	{
+		check_buttons_();
+		draw_menu();
+	}
+}
+
--- Bomberman/Menu2/Menu.h
+++ Bomberman/Menu2/Menu.h
@@ -1,3 +1,16 @@
 #ifndef Menu_H
 #define Menu_H
+
+
+class Menu
+{
+protected:
+	bool is_menu_open_ = false;
+	virtual void check_buttons_(){};
+	void virtual draw_menu(){};
+public:
+	virtual void open_menu();
+};
+
+
 #endif 
