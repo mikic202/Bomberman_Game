acd03492b60fafed40271384e49f709f9e6867e4
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-07-11 20:00:18 +0200

    finished open menu method

--- Bomberman/Menu2/MainMenu.cpp
+++ Bomberman/Menu2/MainMenu.cpp
@@ -1,5 +1,7 @@
 #include "MainMenu.h"
 #include "../Consts.h"
+#include "ChoseTypeMenu.h"
+#include "../Game/Game.h"
 
 
 void MainMenu::genarate_menu_()
@@ -19,12 +21,16 @@ void MainMenu::draw_menu_(sf::RenderWindow& window)
 	window.display();
 }
 
-void MainMenu::check_actions_(sf::RenderWindow& window)
+GameTypeEnum MainMenu::check_actions_(sf::RenderWindow& window)
 {
 	for (auto button_pair : buttons_)
 	{
-
+		if (button_pair.second.is_pressed(window))
+		{
+			return button_pair.first;
+		}
 	}
+	return GameTypeEnum::no_choice;
 }
 
 MainMenu::MainMenu()
@@ -35,3 +41,45 @@ MainMenu::MainMenu()
 	}
 	is_menu_open_ = true;
 }
+
+std::vector<GameTypeEnum> MainMenu::open_menu(sf::RenderWindow& window)
+{
+	genarate_menu_();
+	while (is_menu_open_)
+	{
+		draw_menu_(window);
+		check_quit_(window);
+		GameTypeEnum chosen_mode = check_actions_(window);
+		if (chosen_mode != GameTypeEnum::no_choice)
+		{
+			std::vector<GameTypeEnum> return_choices = { GameTypeEnum::no_choice, GameTypeEnum::no_choice };
+			ChoseTypeMenu type_menu;
+			switch (chosen_mode)
+			{
+			case GameTypeEnum::story:
+				return_choices = type_menu.open_menu(window);
+				break;
+			case GameTypeEnum::coop:
+				return_choices = type_menu.open_menu(window);
+				break;
+			case GameTypeEnum::versus:
+				break;
+			};
+			int save_number = 1;
+
+			switch (return_choices[1])
+			{
+			case GameTypeEnum::second_s:
+				save_number = 2;
+				break;
+			case GameTypeEnum::third_s:
+				save_number = 2;
+				break;
+			}
+			Game game;
+			game.play(save_number, chosen_mode, return_choices[0], window);
+		}
+	}
+	return std::vector<GameTypeEnum>();
+}
+
