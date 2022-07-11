a1f13714a79b2582fd56be5660415e627d8ee0b4
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-07-11 14:33:33 +0200

    added chose save menu class

---
+++ Bomberman/Menu2/ChoseSaveMenu.cpp
@@ -0,0 +1,1 @@
+#include "ChoseSaveMenu.h"
---
+++ Bomberman/Menu2/ChoseSaveMenu.h
@@ -0,0 +1,13 @@
+#ifndef ChoseSaveMenu_H
+#define ChoseSaveMenu_H
+#include "Menu.h"
+
+
+class ChoseSaveMenu : Menu
+{
+
+};
+
+
+#endif // !ChoseSaveMenu_H
+
