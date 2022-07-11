c84e7ff2f88239daa75c7d2e7bcc64004eb4e067
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-07-11 15:21:37 +0200

    fixed text centering

--- Bomberman/Menu2/Button.cpp
+++ Bomberman/Menu2/Button.cpp
@@ -7,7 +7,7 @@ Button::Button(sf::Vector2f position, int size, sf::Color color, std::string tex
 	button_text_.setString(text);
 	button_text_.setCharacterSize(size);
 	button_text_.setFillColor(color);
-	auto center = button_text_.getGlobalBounds().left / 2.f;
+	auto center = button_text_.getLocalBounds().width / 2.f;
 	button_text_.setOrigin({ center, button_text_.getGlobalBounds().top });
 	button_text_.setPosition(position);
 }
