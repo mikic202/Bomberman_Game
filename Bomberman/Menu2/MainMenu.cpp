c621ed757cb79edc5f090e351bf8955c5666b94f
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-07-11 15:21:52 +0200

    changed buttons positioning

--- Bomberman/Menu2/MainMenu.cpp
+++ Bomberman/Menu2/MainMenu.cpp
@@ -4,14 +4,14 @@
 
 void MainMenu::genarate_menu_()
 {
-	buttons_.insert(std::pair<int, Button>(1, Button({ WINDOW_WIDTH / 2, 200.f }, 20, sf::Color::Yellow, "Play Story", font_)));
-	buttons_.insert(std::pair<int, Button>(2, Button({ WINDOW_WIDTH / 2, 300.f }, 20, sf::Color::Yellow, "Play Cooperation", font_)));
-	buttons_.insert(std::pair<int, Button>(3, Button({ WINDOW_WIDTH / 2, 400.f }, 20, sf::Color::Yellow, "Play Versus", font_)));
+	buttons_.insert(std::pair<int, Button>(1, Button({ WINDOW_WIDTH / 2, 250.f }, 50, sf::Color::Yellow, "Play Story", font_)));
+	buttons_.insert(std::pair<int, Button>(2, Button({ WINDOW_WIDTH / 2, 400.f }, 50, sf::Color::Yellow, "Play Cooperation", font_)));
+	buttons_.insert(std::pair<int, Button>(3, Button({ WINDOW_WIDTH / 2, 560.f }, 50, sf::Color::Yellow, "Play Versus", font_)));
 }
 
 void MainMenu::draw_menu_(sf::RenderWindow& window)
 {
-	window.clear(sf::Color::Green);
+	window.clear(sf::Color(69, 159, 66));
 	for (auto buttons : buttons_)
 	{
 		buttons.second.draw_to(window);
