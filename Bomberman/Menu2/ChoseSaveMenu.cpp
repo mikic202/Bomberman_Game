ad61c87471202f64310ba2ce55bc4708800ccc89
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-07-15 14:24:31 +0200

    created chose save menu and implemented all its methods

--- Bomberman/Menu2/ChoseSaveMenu.cpp
+++ Bomberman/Menu2/ChoseSaveMenu.cpp
@@ -1,1 +1,45 @@
 #include "ChoseSaveMenu.h"
+
+void ChoseSaveMenu::genarate_menu_()
+{
+    buttons_.insert(std::pair<GameTypeEnum, Button>(GameTypeEnum::first_s, Button({ WINDOW_WIDTH / 2, 250.f }, 50, sf::Color::Yellow, "Save 1", font_)));
+    buttons_.insert(std::pair<GameTypeEnum, Button>(GameTypeEnum::second_s, Button({ WINDOW_WIDTH / 2, 400.f }, 50, sf::Color::Yellow, "Save 2", font_)));
+    buttons_.insert(std::pair<GameTypeEnum, Button>(GameTypeEnum::third_s, Button({ WINDOW_WIDTH / 2, 560.f }, 50, sf::Color::Yellow, "Save 3", font_)));
+}
+
+void ChoseSaveMenu::draw_menu_(sf::RenderWindow& window)
+{
+    window.clear(sf::Color(69, 159, 66));
+    for (auto buttons : buttons_)
+    {
+        buttons.second.draw_to(window);
+    }
+    window.display();
+}
+
+ChoseSaveMenu::ChoseSaveMenu()
+{
+    if (!font_.loadFromFile(FONT_PATH))
+    {
+        throw(std::exception());
+    }
+    is_menu_open_ = true;
+}
+
+std::vector<GameTypeEnum> ChoseSaveMenu::open_menu(sf::RenderWindow& window)
+{
+    check_if_mouse_still_pressed_();
+    GameTypeEnum chosen_save = GameTypeEnum::no_choice;
+    genarate_menu_();
+    while (is_menu_open_)
+    {
+        check_quit_(window);
+        chosen_save = check_actions_(window);
+        draw_menu_(window);
+        if (chosen_save != GameTypeEnum::no_choice)
+        {
+            is_menu_open_ = false;
+        }
+    }
+    return { chosen_save };
+}
--- Bomberman/Menu2/ChoseSaveMenu.h
+++ Bomberman/Menu2/ChoseSaveMenu.h
@@ -3,9 +3,14 @@
 #include "Menu.h"
 
 
-class ChoseSaveMenu : Menu
+class ChoseSaveMenu : public Menu
 {
-
+private:
+	void genarate_menu_();
+	void draw_menu_(sf::RenderWindow& window);
+public:
+	ChoseSaveMenu();
+	std::vector<GameTypeEnum> open_menu(sf::RenderWindow& window);
 };
 
 
