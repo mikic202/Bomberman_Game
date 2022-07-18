824191457f2944197aba4d2b7afcb6fe6e8e70d4
Mikołaj Chomanski <01168876@pw.edu.pl>
2022-07-18 18:32:42 +0200

    created game

--- Bomberman/main.cpp
+++ Bomberman/main.cpp
@@ -4,15 +4,13 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include "Menu2/MainMenu.h"
+#include "Consts.h"
 
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(825, 825), "Bomberman");
 	MainMenu menu;
 	menu.open_menu(window);
-	//MainMenu* mainmenu = new MainMenu();
-	//mainmenu->run();
-	//delete mainmenu;
 	std::cout << "a";
 
 }
