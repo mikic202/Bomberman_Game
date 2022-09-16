097afb6aa2e867510d18f614f358e18154ff3cbc
mikic202 <mikolaj.chomanski@gmail.com>
2022-09-16 18:11:27 +0200

    changed how often boxes spawn and how strong they are

--- Bomberman/Game Board/StoryModeBoard.h
+++ Bomberman/Game Board/StoryModeBoard.h
@@ -13,8 +13,8 @@ private:
 	int level_number_;
 	Door door_;
 	const int where_door_can_ocure = 50;
-	std::map<int, int> dificulty_map_ {{0, 50}, {5, 70}, {10, 90}};// first - levels pased, second - boxes on board
-	std::map<int, int> box_strength_map_{ {0, 1}, {5, 2}, {10, 3} };
+	std::map<int, int> dificulty_map_ {{0, 40}, {8, 50}, {16, 60}};// first - levels pased, second - boxes on board
+	std::map<int, int> box_strength_map_{ {0, 1}, {10, 2} };
 	void generate_board_(sf::Texture& wall_texture, sf::Texture& box_texture, sf::Texture& door_texture);
 	int box_num_(int level_num) const;
 	int max_box_strength_(int level_num) const;
